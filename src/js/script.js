61af40bc9d95d0bd75412aa764f4dddadd784f73
Tristan Harward <trisweb@gmail.com>
2019-01-13 22:45:17 -0500

    Better typography, and implement my random color detail.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,113 @@
+
+// Color functions
+/////////////////////////////////////////////////////////////////
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Turn HSL color components into RGB.
+function hslToRgb(h, s, l) {
+  h = h / 360;
+  s = s / 100;
+  l = l / 100;
+
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // Achromatic
+  } else {
+    var hue2rgb = function hue2rgb(p, q, t){
+      if(t < 0) t += 1;
+      if(t > 1) t -= 1;
+      if(t < 1/6) return p + (q - p) * 6 * t;
+      if(t < 1/2) return q;
+      if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+// Calculate the perceptual luminance of a color, based on how our eyeballs work.
+// (we see green the best, followed by red, and blue the least)
+// Just blatantly ripped out of a stackoverflow answer, which was itself based
+// on an algorithm from a w3c spec.
+function luminanace(r, g, b) {
+  var a = [r, g, b].map(function (v) {
+      v /= 255;
+      return v <= 0.03928
+          ? v / 12.92
+          : Math.pow( (v + 0.055) / 1.055, 2.4 );
+  });
+  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
+
+function colorContrast(hsl1, hsl2) {
+  var rgb1 = hslToRgb(hsl1[0], hsl1[1], hsl1[2]);
+  var rgb2 = hslToRgb(hsl2[0], hsl2[1], hsl2[2]);
+
+  var l1 = luminanace(rgb1[0], rgb1[1], rgb1[2]) + 0.05;
+  var l2 = luminanace(rgb2[0], rgb2[1], rgb2[2]) + 0.05;
+
+  // Divide the lum of the lighter color by the brighter.
+  return l1 > l2 ? l1 / l2 : l2 / l1;
+}
+
+///
+
+function setRandomColors() {
+	var randomHue = getRandomInt(0, 360);
+
+	var lightness = 100;
+	var MIN_CONTRAST = 3;
+
+	while (colorContrast([randomHue, 100, lightness], [0, 0, 100]) < MIN_CONTRAST) {
+    lightness = lightness - 1;
+    if (lightness == 0) break;
+  }
+
+	var randomColor = "hsl("+randomHue+", 100%, "+lightness+"%)";
+	setTimeout(function() {
+		$("h1.blog-name").css("color", randomColor);
+	}, 100);
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
 jQuery(function($) {
 
 	var body = $('body');
 	var html = $('html');
 	var viewport = $(window);
+
+	/* ========================================================================== */
+	/* TRISTAN: Title color. */
+	// While my main page does foreground and background, for a blog that would be
+	// pretty damn annoying. So I'll just do a random color for the main title here.
+
+	setRandomColors();
+	// On click anywhere, do it again!
+	$(body).on("click touch", function() {
+		setRandomColors();
+	});
+
+	/* TRISTAN END */
+
 
 	/* ==========================================================================
 	   Menu
